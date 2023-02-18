808e3cfe31d3d627ee248e8d1be96459ef4a6cf2
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-18 17:52:03 +0100

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-creation of model named User for a database table named users
-using the mapping declaration of SQLAlchemy
+""" User module
 """
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -14,9 +12,17 @@ class User(Base):
     """
     User class.
     """
+
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        """
+        String rep.
+        """
+        return f"User: id={self.id}"
